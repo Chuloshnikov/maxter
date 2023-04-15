4bd4204cb2002f191bdbf52c7c76ca618c0077e2
MaxChuloshnikov <Maks447@ukr.net>
2023-04-15 22:03:51 +0300

    GetProduct done + Header
    Images fix need

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -1,12 +1,14 @@
 import nc from "next-connect";
 import dbConnect from "../../../../db/dbConnect";
 import { newProduct } from "../../../../controllers/productControlers";
+import { getProducts } from "../../../../controllers/productControlers";
 
 
 const handler = nc();
 
 dbConnect();
 
+handler.get(getProducts);
 handler.post(newProduct);
 
 export default handler;
